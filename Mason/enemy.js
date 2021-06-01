c28006ca8c392d08723e176d8e7e4c4ff45c38f8
Zekromeon <60363429+Zekromeon@users.noreply.github.com>
2021-06-01 10:19:04 -0400

    Update enemy.js

--- Mason/enemy.js
+++ Mason/enemy.js
@@ -1,11 +1,11 @@
-class enemy
+class Enemy
 {
 	constructor(x, y, health, speed, sourceImage)
 	{
-	  this.x = x
-	  this.y = y
-	  this.health = health
-	  this.speed = speed
-	  this.sourceImage = sourceImage
+	  this.x = x;
+	  this.y = y;
+	  this.health = health;
+	  this.speed = speed;
+	  this.sourceImage = sourceImage;
 	}
 }
