d57c51521ac70ca3af75abf9d01a4c9ebbf6ab84
Zekromeon <60363429+Zekromeon@users.noreply.github.com>
2021-06-01 10:17:30 -0400

    Create enemy.js

---
+++ Mason/enemy.js
@@ -0,0 +1,11 @@
+class enemy
+{
+	constructor(x, y, health, speed, sourceImage)
+  {
+  	this.x = x
+    this.y = y
+    this.health = health
+    this.speed = speed
+    this.sourceImage = sourceImage
+  }
+}
