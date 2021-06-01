f03ad8285afb8170f6dab9fd82a948bf1e2ee351
Donato Fiore <50346119+donato-fiore@users.noreply.github.com>
2021-06-01 10:24:23 -0400

    remove random a in a line, fix indentation

--- donto/Player.js
+++ donto/Player.js
@@ -1,32 +1,30 @@
 class Player {
 	constructor(health, money) {
-  	this.health = health;
-    this.money = money;
-    this.unlocked = [];
-  }
-  
-  getHealth() {
-  	return this.health;
-  }
-  
-  getMoney() {
-  	return this.money;
-  }
-  
-  getUnlocked() {
-  	return this.unlocked;
-  }
-  
-  addHealth(h) {
-  	this.health = this.health + h;
-  }
-  
-  removeHealth(h) {
-  	this.health = this.health - h;
-  }
-  
-  addToUnlocked(thing) {
-  	this.unlocked.push(thing);a
-  }
-  
+		this.health = health;
+    		this.money = money;
+    		this.unlocked = [];
+	}
+	getHealth() {
+		return this.health;
+	  }
+
+	  getMoney() {
+		return this.money;
+	  }
+
+	  getUnlocked() {
+		return this.unlocked;
+	  }
+
+	  addHealth(h) {
+		this.health = this.health + h;
+	  }
+
+	  removeHealth(h) {
+		this.health = this.health - h;
+	  }
+
+	  addToUnlocked(thing) {
+		this.unlocked.push(thing);
+	  }
 }
