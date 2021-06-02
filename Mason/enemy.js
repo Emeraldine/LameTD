9b388d4fc2edcb8a46fa94b8c9bddb4c2d5aabec
Zekromeon <60363429+Zekromeon@users.noreply.github.com>
2021-06-02 10:22:15 -0400

    Update enemy.js

--- Mason/enemy.js
+++ Mason/enemy.js
@@ -2,12 +2,12 @@ class Enemy
 {
 	constructor(x, y, health, speed, reward, sourceImage)
 	{
-	  this.x = x;
-	  this.y = y;
-	  this.health = health;
-	  this.speed = speed;
-    this.reward = reward;
-	  this.sourceImage = sourceImage;
+		this.x = x;
+		this.y = y;
+		this.health = health;
+		this.speed = speed;
+		this.reward = reward;
+		this.sourceImage = sourceImage;
 	}
   
   getHealth()
@@ -47,8 +47,8 @@ class Enemy
   	var base_image = new Image();
   	base_image.src = this.sourceImage;
   	base_image.onload = function()
-    {
-    	ctx.drawImage(base_image, 0, 0, dimX, dimY);
-  	}
+		{
+			ctx.drawImage(base_image, 0, 0, dimX, dimY);
+		}
   }
 }
