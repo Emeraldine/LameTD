44ff8ac2915239225257605b73bdb53050cafc67
Pawel456 <84788988+Pawel456@users.noreply.github.com>
2021-06-04 10:23:59 -0400

    Create Tower.js

---
+++ Pawel/Tower.js
@@ -0,0 +1,33 @@
+class Tower
+{
+	constructer(atkdmg, atkspd, atkrange)
+  {
+  	this.atkdmg = atkdmg;
+    this.atkspd = atkspd;
+    this.atkrange = atkrange;
+  }
+  getatkdmg()
+  {
+  	return this.atkdamage;
+  }
+  getatkspd()
+  {
+  	return this.atkspd;
+  }
+  getatkrange()
+  {
+  	return this.atkrange;
+  }
+  setatkdmg(val)
+  {
+  	this.atkdmg = val;
+  }
+  setatkspd(val)
+  {
+  	this.atkspd = val;
+  }
+  setatkrange(val)
+  {
+  	this.atkrange = val;
+  }
+  
