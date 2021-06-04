65684c84561a36306c57833d88c7f704b97d94b2
Zekromeon <60363429+Zekromeon@users.noreply.github.com>
2021-06-04 10:16:24 -0400

    Update tmpenemy.js

--- donto/tmpenemy.js
+++ donto/tmpenemy.js
@@ -6,7 +6,8 @@ canvas.onload = function(){update()};
 
 function update() {
   ctx.beginPath();
-  ctx.arc(100, 100, 50, 0, 2 * Math.PI, true);
+  //ctx.arc(100, 100, 50, 0, 2 * Math.PI, true);
+  ctx.rect(100, 100, 50, 100);
   ctx.fillStyle = "#FF6A6A";
   ctx.fill();
  
