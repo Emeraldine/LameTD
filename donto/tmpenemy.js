a0845e2734646571b19ee774c69f68c0a10cb653
Donato Fiore <50346119+donato-fiore@users.noreply.github.com>
2021-06-04 10:03:05 -0400

    Update tmpenemy.js

--- donto/tmpenemy.js
+++ donto/tmpenemy.js
@@ -2,10 +2,10 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
 function update() {
-  context.beginPath();
-  context.arc(100, 100, 50, 0, 2 * Math.PI, true);
-  context.fillStyle = "#FF6A6A";
-  context.fill();
+  ctx.beginPath();
+  ctx.arc(100, 100, 50, 0, 2 * Math.PI, true);
+  ctx.fillStyle = "#FF6A6A";
+  ctx.fill();
  
   requestAnimationFrame(update);
 }
