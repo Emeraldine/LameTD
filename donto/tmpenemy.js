1d9747f44fea42f0e40a0097fcddb0ac0cef8583
mfpankau <74994673+mfpankau@users.noreply.github.com>
2021-06-07 10:16:13 -0400

    Update tmpenemy.js

--- donto/tmpenemy.js
+++ donto/tmpenemy.js
@@ -1,23 +1,11 @@
-
-var canvas = document.getElementById("canvas");
-canvas.onload = function(){update()};
-
-
 function update(canvas) {
   
   var ctx = canvas.getContext("2d");
   ctx.beginPath();
-  //ctx.arc(100, 100, 50, 0, 2 * Math.PI, true);
-  ctx.rect(100, 100, 50, 100);
+  ctx.arc(100, 100, 50, 0, 2 * Math.PI, true);
   ctx.fillStyle = "#FF6A6A";
-  ctx.stroke();
-  //ctx.fill();
+  ctx.fill();
  
   requestAnimationFrame(update);
 }
-function doThing()
-{
-  alert("bad");
-}
-
 update();   
