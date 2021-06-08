dae0fc1e2cfecaa9273c3f0c58c4a0cb9f0f0de3
Zekromeon <60363429+Zekromeon@users.noreply.github.com>
2021-06-08 10:24:50 -0400

    Update enemy.js

--- Mason/enemy.js
+++ Mason/enemy.js
@@ -68,13 +68,23 @@ function makeT2Enemy(x, y)
 	t2Enemy.drawEnemy(32, 32);
 }
 
-function makeT1Enemy(x, y)
+function makeT3Enemy(x, y)
 {
 	var t3Enemy = new enemy(x, y, 24, 15, 30, "../logan/Enemy%203.png");
 	t3Enemy.drawEnemy(32, 32);
 }
 
-/*function makeT1Enemy(x, y)
+function makeT4Enemy(x, y)
 {
-	var t1Enemy = new enemy(x, y, 3, 5, 10, "../logan/Enemy%204.png");
-}*/
+	var t14nemy = new enemy(x, y, 36, 20, 40, "../logan/Enemy%204.png");
+}
+
+function makeT5Enemy(x, y)
+{
+	var t5Enemy = new enemy(x, y, 45, 25, 50, "../logan/Enemy%205.png");
+}
+
+function makeT6Enemy(x, y)
+{
+	var t6Enemy = new enemy(x, y, 57, 30, 60, "../logan/Enemy%206.png");
+}
